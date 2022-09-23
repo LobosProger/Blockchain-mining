94619a30e77db2d90ad072ec460f6f16b71fd1f0
LobosProger <78168123+LobosProger@users.noreply.github.com>
2022-09-23 21:57:53 +0600

    Add files via upload

--- Blockchain.cs
+++ Blockchain.cs
@@ -2,154 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Text;
 using System.Security.Cryptography;
-
+using System.Text;
 public class Blockchain : MonoBehaviour
 {
-	[SerializeField]
-	private string data;
-	[SerializeField]
-	private string difficulty;
-
-
-	[Space(20)]
-	public List<Block> blockchain = new List<Block>();
+	public string difficulty = "0";
+	public string Data;
+	public List<Block> BlockchainInfo = new List<Block>();
 
 	[SerializeField]
-	private bool mine;
-	private void Start()
-	{
-		if (blockchain.Count == 0)
-		{
-			Block genesisBlock = new Block(0, "Genesis block", "0");
-			genesisBlock.mineBlock();
-			genesisBlock.assignDataToBlock();
-
-			blockchain.Add(genesisBlock);
-		}
-	}
+	private bool Mine;
 
 	private void Update()
 	{
-		if (mine)
+		if (Mine)
 		{
-			mine = false;
+			Mine = false;
 			StartCoroutine(Mining());
+		}
+	}
+
+	[Serializable]
+	public struct Block
+	{
+		public int block;
+		public ulong nonce;
+		public string Data;
+		public string previousHash;
+		public string Hash;
+
+		[Space(40)]
+		public double TimeOfMining;
+
+		private string connectedData;
+		public string hashingDataWithNonce;
+
+		public void AssignDataToBlock(string data, List<Block> blockchain)
+		{
+			nonce = 0;
+			Hash = "a";
+			block = blockchain.Count + 1;
+			Data = data;
+			previousHash = blockchain.Count > 0 ? blockchain[block - 2].Hash : "0";
+
+			connectedData = block.ToString() + data + previousHash;
+		}
+
+		public void MineTheBlock()
+		{
+			nonce++;
+			hashingDataWithNonce = connectedData + nonce.ToString();
+			Hash = EncryptSHA256(hashingDataWithNonce);
+		}
+
+		private string EncryptSHA256(string Data)
+		{
+			SHA256 sha256 = new SHA256CryptoServiceProvider();
+			sha256.ComputeHash(ASCIIEncoding.ASCII.GetBytes(Data));
+			byte[] result = sha256.Hash;
+			StringBuilder strBuilder = new StringBuilder();
+			for (int i = 0; i < result.Length; i++)
+			{
+				strBuilder.Append(result[i].ToString("x2"));
+			}
+			return strBuilder.ToString();
 		}
 	}
 
 	IEnumerator Mining()
 	{
-		Block miningBlock = new Block(blockchain.Count, data, blockchain[blockchain.Count - 1].validHash);
-		miningBlock.mineBlock();
+		Block addingBlock = new Block();
+		addingBlock.AssignDataToBlock(Data, BlockchainInfo);
 
-		while (!hasStringBuilderSubset(miningBlock.validHashStringBuilder, difficulty))
+		addingBlock.TimeOfMining = Time.realtimeSinceStartupAsDouble;
+		int difficultyAmountOfNone = difficulty.Length;
+		addingBlock.MineTheBlock();
+
+		int minedHashes = 0;
+		while (addingBlock.Hash.Substring(0, difficultyAmountOfNone) != difficulty)
 		{
-			for (int i = 0; i <= 200000; i++)
+			addingBlock.MineTheBlock();
+			minedHashes++;
+
+			if (minedHashes > 1000000)
 			{
-				if (hasStringBuilderSubset(miningBlock.validHashStringBuilder, difficulty))
-					break;
-				else
-					miningBlock.mineBlock();
+				Debug.Log("Mining...");
+				minedHashes = 0;
+				GC.Collect();
+				yield return new WaitForSecondsRealtime(3f);
 			}
-
-			Debug.Log("Mining...");
-			GC.Collect();
-			yield return Resources.UnloadUnusedAssets();
-			yield return new WaitForSecondsRealtime(0.05f);
 		}
+		addingBlock.TimeOfMining = Time.realtimeSinceStartupAsDouble - addingBlock.TimeOfMining;
+		BlockchainInfo.Add(addingBlock);
 
-		Debug.Log("Block has been mined!");
-		miningBlock.assignDataToBlock();
-		blockchain.Add(miningBlock);
+		Debug.Log("Block has been mined!\n" + addingBlock.hashingDataWithNonce);
 		yield return null;
-	}
-
-	private bool hasStringBuilderSubset(StringBuilder builder, string subset)
-	{
-		int length = subset.Length;
-		bool hasSubset = true;
-
-		for (int i = 0; i < length; i++)
-		{
-			if (builder[i] != subset[i])
-			{
-				hasSubset = false;
-				break;
-			}
-		}
-
-		return hasSubset;
-	}
-}
-
-[System.Serializable]
-public class Block
-{
-	public int block;
-	public string timestamp;
-	public string data;
-	public string previousHash;
-
-	[Space]
-	public string hashRoot;
-
-	[Space(20)]
-	public string validHash;
-	public StringBuilder validHashStringBuilder;
-	public ulong nonce = 0;
-
-	[Space(70)]
-	public string connectedData;
-	public string hashRootWithNonce;
-	private StringBuilder connectedHashRootWithNonceForMining = new StringBuilder();
-
-	[Space]
-	public double timeOfMining;
-
-	public Block(int block, string data, string previousHash)
-	{
-		this.block = block;
-		this.timestamp = Convert.ToString(DateTime.Now);
-		this.data = data;
-		this.previousHash = previousHash;
-
-		connectedData = string.Concat(block, timestamp, data, previousHash);
-		hashRoot = createSHA256(connectedData).ToString();
-
-		connectedHashRootWithNonceForMining = new StringBuilder();
-		this.timeOfMining = Time.realtimeSinceStartupAsDouble;
-	}
-
-	public void mineBlock()
-	{
-		nonce++;
-		connectedHashRootWithNonceForMining.Clear();
-		connectedHashRootWithNonceForMining.Append(hashRoot);
-		connectedHashRootWithNonceForMining.Append(nonce);
-		validHashStringBuilder = createSHA256(connectedHashRootWithNonceForMining.ToString());
-	}
-
-	public void assignDataToBlock()
-	{
-		validHash = validHashStringBuilder.ToString();
-		hashRootWithNonce = connectedHashRootWithNonceForMining.ToString();
-		timeOfMining = Time.realtimeSinceStartupAsDouble - timeOfMining;
-	}
-
-
-
-	private StringBuilder createSHA256(string Data)
-	{
-		SHA256 sha256 = new SHA256CryptoServiceProvider();
-		sha256.ComputeHash(ASCIIEncoding.ASCII.GetBytes(Data));
-		byte[] result = sha256.Hash;
-		StringBuilder strBuilder = new StringBuilder();
-		for (int i = 0; i < result.Length; i++)
-		{
-			strBuilder.Append(result[i].ToString("x2"));
-		}
-		return strBuilder;
 	}
 }
